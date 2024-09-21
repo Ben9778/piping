f50ba727f9e45eb9db4e5dc1a7984e941f8037a1
beng9778 <ben9778@163.com>
2024-09-21 22:37:44 +0800

    add note

--- src/Define/KeywordDefine.java
+++ src/Define/KeywordDefine.java
@@ -2,6 +2,6 @@ package Define;
 
 public  class KeywordDefine {
     public static final int port=12345;
-    public static final String authToken="12345";
-    public static final String originAddress_KEY="originAddress";
+    public static final String authToken="<Your AuthToken>";
+    public static final String originAddress_KEY="originAddress:";
 }
