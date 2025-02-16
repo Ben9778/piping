8c0c37cb296790139af7426ca36e6570a38f464a
beng9778 <ben9778@163.com>
2025-02-16 12:02:04 +0800

    feat: compile with JDK21

--- src/Define/KeywordDefine.java
+++
@@ -1,7 +0,0 @@
-package Define;
-
-public  class KeywordDefine {
-    public static final int port=12345;
-    public static final String authToken="<Your AuthToken>";
-    public static final String originAddress_KEY="originAddress:";
-}
